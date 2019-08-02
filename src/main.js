5016330b0109f93fd3f60cd67c800e86c7815383
qiqi <13759525126@163.com>
2019-08-02 17:18:32 +0800

    test

--- src/main.js
+++ src/main.js
@@ -1,12 +1,18 @@
-import Vue from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import fastclick from 'fastclick'
 
-Vue.config.productionTip = false;
+import 'assets/stylus/index.styl'
 
+fastclick.attach(document.body)
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
 new Vue({
   router,
   store,
   render: h => h(App)
-}).$mount("#app");
+}).$mount('#app')
---
+++ vue.config.js
@@ -0,0 +1,18 @@
+// vue.config.js
+const path = require('path')
+
+function resolve(dir) {
+  return path.join(__dirname, dir)
+}
+module.exports = {
+  configureWebpack: {
+    resolve: {
+      extensions: ['.js', '.vue', '.json'],
+      alias: {
+        '@': resolve('src'),
+        assets: resolve('src/assets'),
+        components: resolve('src/components')
+      }
+    }
+  }
+}
